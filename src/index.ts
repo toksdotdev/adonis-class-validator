ab7b4a705b31f3f07f3dfaf5b56175ef5a4a3d93
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2021-07-29 10:59:22 +0100

    Feat(Validate): Create Standalone Validator Class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
 export { validate } from "./decorators";
 export { getValidatorBag } from "./utils";
+import { getValidatorBag } from "./utils";
+import { plainToClass } from "class-transformer";
+import { schema } from "@adonisjs/validator/build/src/Schema";
+import { validator } from "@adonisjs/validator/build/src/Validator";
+import { Class, ClassValidatorArg } from "@ioc:Adonis/ClassValidator/Shared";
+
+export class ClassValidate {
+  /**
+   * Validate data using a class schema.
+   * @param validatorClass Validator class.
+   * @param data Data to validate.
+   * @param args Validator config.
+   * @returns Validated data.
+   */
+  public static async validate<T>(
+    validatorClass: Class<T>,
+    data: any,
+    args?: Omit<ClassValidatorArg, "data">
+  ): Promise<T> {
+    const validatorBag = getValidatorBag(validatorClass);
+
+    const validatedData = await validator.validate({
+      schema: schema.create(validatorBag.schema),
+      cacheKey: validatorBag.key,
+      data,
+      ...args,
+    });
+
+    return plainToClass(validatorClass, validatedData);
+  }
+}
--- test/classValidate.spec.ts
+++ test/classValidate.spec.ts
@@ -1,14 +1,14 @@
 import test from "japa";
 import { DateTime } from "luxon";
+import { ClassValidate } from "../src";
 import { validate } from "../src/decorators";
 import { Assert } from "japa/build/src/Assert";
-import { getValidatorBag } from "../src/utils";
-import { plainToClass } from "class-transformer";
+import { AddressPoint, NoSchema } from "./cases/classes";
 import { schema } from "@adonisjs/validator/build/src/Schema";
-import { validator } from "@adonisjs/validator/build/src/Validator/index";
+import { ValidationException } from "@adonisjs/validator/build/src/ValidationException";
 
-test.group("Class Validation and Transform", () => {
-  test("Date field is formatted and transformed correctly", async (assert: Assert) => {
+test.group("ClassValidate", () => {
+  test("ClassValidate.validate(...) formats and transforms correctly", async (assert: Assert) => {
     const dob = "2001-02-12";
     const format = "yyyy-MM-dd";
 
@@ -17,14 +17,39 @@ test.group("Class Validation and Transform", () => {
       public dob!: DateTime;
     }
 
-    const validatorBag = getValidatorBag(DateSchema);
-    const data = await validator.validate({
-      schema: schema.create(validatorBag.schema),
-      cacheKey: validatorBag.key,
-      data: { dob },
-    });
+    const actual = await ClassValidate.validate(DateSchema, { dob });
+    assert.deepEqual(actual, { dob: DateTime.fromFormat(dob, format) });
+  });
 
-    const classData = plainToClass(DateSchema, data);
-    assert.deepEqual(classData.dob, DateTime.fromFormat(dob, format));
+  test("ClassValidate.validate(...) validates correctly", async (assert: Assert) => {
+    const expected: AddressPoint = { uniqueId: "hello world" };
+    const actual = await ClassValidate.validate(AddressPoint, expected);
+    assert.deepEqual(actual, expected);
+  });
+
+  test("ClassValidate.validate(...) fails on invalid input validation", async (assert: Assert) => {
+    assert.plan(1);
+
+    try {
+      const expected = { uniqueId: 2 };
+      await ClassValidate.validate(AddressPoint, expected);
+    } catch (err) {
+      assert.instanceOf(err, ValidationException);
+    }
+  });
+
+  test("ClassValidate.validate(...) fails on empty input validation", async (assert: Assert) => {
+    assert.plan(1);
+
+    try {
+      await ClassValidate.validate(AddressPoint, {});
+    } catch (err) {
+      assert.instanceOf(err, ValidationException);
+    }
+  });
+
+  test("ClassValidate.validate(...) succeeds with empty validation schema", async (assert: Assert) => {
+    const actual = await ClassValidate.validate(NoSchema, { hello: "hi" });
+    assert.deepEqual(actual as any, {});
   });
 });
